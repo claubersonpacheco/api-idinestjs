7f35039740f93f6fe16810729fdf4541493ecab9
caubinho <caubinho@gmail.com>
2026-05-05 23:40:19 +0200

    main II

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://127.0.0.1:8080',
-      'http://SEU_IP:8080',
-      'https://seudominio.com', // quando tiver domínio
-    ],
+    origin: process.env.CORS_ORIGIN?.split(',') || [],
     credentials: true,
   });
 
