bae75b733ec75b16c9023bdcd838f8fca064262c
caubinho <caubinho@gmail.com>
2026-05-05 23:35:09 +0200

    mains.js

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:8080',
+      'http://SEU_IP:8080',
+      'https://seudominio.com', // quando tiver domínio
+    ],
     credentials: true,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,7 +22,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
-}
 
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+}
 void bootstrap();
